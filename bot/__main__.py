16f681349033275e7149824a40b4aae690469ce9
Dalton Neely <daltonjn1@gmail.com>
2021-07-24 22:35:14 -0600

    feat: binance client get BTCUSD symbol

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,11 +4,19 @@ from sys import argv
 key_api = argv[1]
 key_secret = argv[2]
 client = Client(key_api, key_secret, tld='us')
+
 is_running = True
-max_loops = 10
+max_loops = 1
 loop_counter = 0
+
+print('Program starting...')
 while is_running and loop_counter < max_loops:
-    print('Hello, World! I am a bot and I am running. %i' % loop_counter)
+    print('Loop number: %i' % loop_counter)
+
+    # main code
+    exchange_info = client.get_exchange_info()
+    print(exchange_info['symbols'][0]['symbol'])
+
     loop_counter += 1
 else:
-    print('Goodbye, World!')
+    print('Program ending')
