2ac25e678f226b0d53d513fabb6827be1f690e78
Dalton Neely <daltonjn1@gmail.com>
2021-07-24 22:00:32 -0600

    feat: add loop

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,7 @@
-print('Hello, World! I am a bot')
+is_running = True
+loop_counter = 1
+while is_running and loop_counter <= 10:
+    print('Hello, World! I am a bot and I am running. %i' % loop_counter)
+    loop_counter += 1
+else:
+    print('Goodbye, World!')
