6604e91c0f6a95fb31735341e424a0bb3f48b068
Dalton Neely <daltonjn1@gmail.com>
2021-07-24 22:26:20 -0600

    feat: add basic binance us client

--- bot/README.md
+++ bot/README.md
@@ -1,4 +1,7 @@
 # Bot
 ```bash
-python3 bot/bot.py
+python3 -m pip install python-binance
+```
+```bash
+python3 bot/bot.py <key_api> <key_secret>
 ```
--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,13 @@
+from binance.client import Client
+from sys import argv
+
+key_api = argv[1]
+key_secret = argv[2]
+client = Client(key_api, key_secret)
 is_running = True
-loop_counter = 1
-while is_running and loop_counter <= 10:
+max_loops = 10
+loop_counter = 0
+while is_running and loop_counter < max_loops:
     print('Hello, World! I am a bot and I am running. %i' % loop_counter)
     loop_counter += 1
 else:
