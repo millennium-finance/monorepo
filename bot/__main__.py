44e6d5584657041640f10201540bbf4e8deb18cf
Dalton Neely <daltonjn1@gmail.com>
2021-07-24 22:29:52 -0600

    feat: change binance client to use US tld

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,7 +3,7 @@ from sys import argv
 
 key_api = argv[1]
 key_secret = argv[2]
-client = Client(key_api, key_secret)
+client = Client(key_api, key_secret, tld='us')
 is_running = True
 max_loops = 10
 loop_counter = 0
